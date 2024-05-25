b93c2eaa33ad860f4d4f4bf869f4526bc702166f
Yahg2005 <161893815+Yahg2005@users.noreply.github.com>
2024-05-25 21:09:42 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+function TrafficLight() {
+	const [color, setColor] = useState("red");
+  
+	const toggleColor = () => {
+	  if (color === 'red') {
+		setColor('yellow');
+	  } else if (color === 'yellow') {
+		setColor('green');
+	  } else if (color === 'green') {
+		setColor('red');
+	  }
+	};
+  
+	const addPurple = () => {
+	  setColor('purple');
+	};
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className={`center`}>	
+		<div className={`black-line`} >
+		/</div>
+	  <div className="trafficlight d-flex flex-column align-items-center mt-5">
+		<div 
+		  className={`Light red ${color === 'red' ? 'glow' : ''}`} 
+		  onClick={() => setColor('red')}
+		></div>
+		<div 
+		  className={`Light yellow ${color === 'yellow' ? 'glow' : ''}`} 
+		  onClick={() => setColor('yellow')}
+		></div>
+		<div 
+		  className={`Light green ${color === 'green' ? 'glow' : ''}`} 
+		  onClick={() => setColor('green')}
+		></div>
+		<div 
+		  className={`Light purple ${color === 'purple' ? 'glow' : ''}`} 
+		  onClick={() => setColor('purple')}
+		></div>
+		<button className="btn btn-primary mt-3" onClick={toggleColor}>
+		  Toggle Color
+		</button>
+		<button className="btn btn-secondary mt-3" onClick={addPurple}>
+		  Add Purple
+		</button>
+	  </div>
+	 </div>
 	);
-};
-
-export default Home;
+  }
+  
+  export default TrafficLight;
+  
